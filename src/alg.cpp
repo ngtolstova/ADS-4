8621e9666bd55d99c772ab1391c932f572e86072
ngtolstova <124882480+ngtolstova@users.noreply.github.com>
2023-03-18 01:14:24 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,48 +1,49 @@
 // Copyright 2021 NNTU-CS
 int countPairs1(int *arr, int len, int value) {
-  int count = 0;
-  for (int i = 0; i < len - 1; i++) {
-    for (int j = i + 1; j < len; j++) {
-      if (arr[i] + arr[j] == value)
-	count++;
-    }
-  }
-  return count;
+	int count = 0;
+	for (int i = 0; i < len - 1; i++) {
+		for (int j = i + 1; j < len; j++) {
+			if (arr[i] + arr[j] == value)
+				count++;
+		}
+	}
+	return count;
 }
 int countPairs2(int *arr, int len, int value) {
-  int count = 0;
-  for (int i = 0; i < len - 1; i++) {
-    for (int j = len - 1; j > i; j--) {
-      if (arr[i] + arr[j] == value)
-	count++;
-    }
-  }
+	int count = 0, k = len - 1;
+	while (arr[k] > value) 
+		k--;
+	for (int i = 0; i < k; i++) {
+		for (int j = k; j > i; j--) {
+			if (arr[i] + arr[j] == value)
+				count++;
+		}
+	}
   return count;
 }
 int countPairs3(int *arr, int len, int value) {
-  int count = 0;
-  for (int i = 0; i < (len - 1); i++) {
-    int start = i;
-    int stop = len - 1;
-    while (start < stop) {
-      int mid = (start + stop) / 2;
-      if (arr[i] + arr[mid] == value) {
-        count++;
-        for (int j = mid + 1; (j < stop) 
-	     && (arr[i] + arr[j] == value) 
-	     && (mid != i) ; j++) {
-	     count++;
-        }
-        for (int j = mid - 1; (j > start) 
-	     && (arr[i] + arr[j] == value) 
-	     && (mid != i); j--) {
-	     count++;
-        }
-        break;
-      }
-      if (arr[i] + arr[mid] > value) stop = i;
-      else start = mid;
-    }
- }
- return count;
+	int count = 0;
+	for (int i = 0; i < len - 1; i++) {
+		int start = i, stop = len;
+		while (start < stop - 1) {
+			int mid = (start + stop) / 2;
+			if (i != mid && (arr[i] + arr[mid] == value)) {
+				count++;
+				for (int j = mid + 1; (j < stop)
+						 && (arr[i] + arr[j] == value)
+						 && (mid != i); j++) {
+					count ++;
+				}
+				for (int j = mid - 1; (j > start)
+						 && (arr[i] + arr[j] == value)
+						 && (mid != i); j--) {
+					count++;
+				}
+				break;
+			}
+			if (arr[i] + arr[mid] > value) stop = i;
+			else start = mid;
+		}
+	}
+	return count;  
 }
